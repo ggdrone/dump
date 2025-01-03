3c7c3ca89828f31526cfbba930530ee80c0cfbe2
ggdrone <ggdrone99@gmail.com>
2025-01-03 11:06:48 +0100

    Added borders and colors

--- tempfan.c
+++ tempfan.c
@@ -2,6 +2,9 @@
    more C and using ncurses */
 
 #include <ncurses.h>
+#include <unistd.h> // For sleep?
+#include <string.h>
+#include <stdlib.h>
 
 // Function for reading CPU temperatur from file.
 int readTemp()
@@ -12,8 +15,7 @@ int readTemp()
   if (fp == NULL)
   {
     // TO DO: add error message
-    return -1;
-  }
+    return -666;  }
 
   char buffer[256];
   if (fgets(buffer, sizeof(buffer), fp))
@@ -25,11 +27,11 @@ int readTemp()
   {
     // TO DO: add error message
     fclose(fp);
-    return -1;
+    return -666;
   } 
 }
 
-// Function for printing temperature to screen
+// Function for printing temperature to screen.
 float printTemp(int x, int y)
 {
   float temp = readTemp(); // Get the temperature.
@@ -48,26 +50,64 @@ float printTemp(int x, int y)
   return 0;
 }
 
+// Function for drawing border.
+void drawBorderAndMessage(int x, int y, const char *message)
+{
+  int len   = strlen(message);
+  int width = len + 16; // Padding, change if needed.
+
+  // Colors, colors, colors...
+  attron(COLOR_PAIR(1));
+  // Draw border with message in it .
+  move(0, 0);
+  for (int i = 0; i < 7; i++)
+  {
+    addch('#');
+  }
+  move(x, y);
+  printw("%s", message);
+
+  move(x, y+len+1);
+  for (int i = 0; i < 7; i++)
+  {
+    addch('#');
+  }
+  
+  //Draw bottom border.
+  move(4, 0);
+  for (int i = 0; i < width; i++)
+  {
+    addch('#');
+  }
+  attroff(COLOR_PAIR(1));
+}
+
+
 int main(void)
 {
   initscr();
-  keypad(stdscr, TRUE); // True means to enable.
+  keypad(stdscr, TRUE);  // True means to enable.
   noecho();
   nodelay(stdscr, TRUE); // Black magic...
+  start_color();         // Starting color festival.
+
+  // Define color pair 1: Red text, default background.
+  init_pair(1, COLOR_RED, COLOR_BLACK);
 
   // Variables for closing program
   // and setting location of text
-  int key = 0;
-  int x   = 2;
-  int y   = 3;
+  int key      = 0;
+  int temp_x   = 2;
+  int temp_y   = 3;
 
   while (key != 'q')
   {
     clear();
-    move(0, 0);
-    printw("Press 'Q' to quit.");
 
-    printTemp(x, y);
+    // Trying to draw border and display message
+    drawBorderAndMessage(0, 8, "Press 'Q' to quit.");
+
+    printTemp(temp_x, temp_y);
 
     sleep(1);
     key = getch(); // Get user input.
